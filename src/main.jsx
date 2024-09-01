d630a2047358df062153761814c9a649e3019003
Kamal Agrahari <kamalagrahari2003@gmail.com>
2024-09-01 15:59:17 +0530

    added kamal.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import Kamal from './Kamal.jsx'
 import './index.css'
 
