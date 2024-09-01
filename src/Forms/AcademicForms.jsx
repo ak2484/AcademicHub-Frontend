ffd77095e93537aaa69f541ddec926895bfae867
Rutvik Gondekar <gondekarrutvik@gmail.com>
2024-09-01 15:38:40 +0530

    add comments detailing fields for forms

--- src/Forms/AcademicForms.jsx
+++ src/Forms/AcademicForms.jsx
@@ -1,15 +1,54 @@
-// export function CourseForm() {}
+/*
+  CourseForm - for creating courses
+  fields: name (required, textbox), courseCode (required, textbox), year (required, number), semester (required, number), credits (required, number), department (required, selection[available department]), teachersIncharge (multiple selection [from avialable teaching staff])
+*/
+export function CourseForm() {}
 
-// export function LabForm() {}
+//////////////////////////////////////////////////////////////////////////////////////////
+/*
+  LabForm - for creating labs
+  fields: name (required, textbox), labCode (required, textbox), credits (required, number), year (required, number), semester (required, number), course (required, selection[available course]), department (required, selection[available department]), teachersIncharge (multiple selection [from avialable teaching staff]), labAssistants (multiple selection [from avialable non-teaching staff]).
+ */
+export function LabForm() {}
 
-// export function AssignmentForm() {}
+//////////////////////////////////////////////////////////////////////////////////////////
+/*
+  AssignmentForm - for creating assignments
+  fields: name (required, textbox), instruction (required, textbox), dateOfPerformance (required, date), dateOfSubmission (required, date), subjectType (required, selection[Course, Lab]), subject (required, selection[available course or lab]), marks (required, number).
+*/
+export function AssignmentForm() {}
 
-// export function ExperimentForm() {}
+//////////////////////////////////////////////////////////////////////////////////////////
+/*
+  ExperimentForm - for creating experiments
+  fields: name (required, textbox), instruction (required, textbox), dateOfPerformance (required, date), dateOfSubmission (required, date), subject (required, selection[available labs]), marks (required, number).
+*/
+export function ExperimentForm() {}
 
-// export function ExamForm() {}
+//////////////////////////////////////////////////////////////////////////////////////////
+/*
+  ExamForm - for creating examinations
+  fields: name (required, textbox), description (textbox), subjectType (required, selection[Course, Lab]), subject (required, selection[available course or lab]), year (required, number), semester (required, number), examDate (required, date), marks (required, number), startTime (required, time), endTime (required, time).
+*/
+export function ExamForm() {}
 
-// export function ExamScoreForm() {}
+//////////////////////////////////////////////////////////////////////////////////////////
+/*
+  ExamScoreForm - for creating exam scores
+  fields: exam (required, selection[available exam]), student (required, selection[available student]), marks (required, number), remark (textbox).
+ */
+export function ExamScoreForm() {}
 
-// export function SubmissionForm() {}
+//////////////////////////////////////////////////////////////////////////////////////////
+/*
+  SubmissionForm - for creating submissions
+  fields: student (required, selection[available student]), subjectType (required, selection[Experiment, Assignment]), subject (required, selection[available experiment or assignment]), dateOfSubmission (required, date), status (required, selection[Submitted, Not Submitted, Graded, Late]), submissionFile (textbox), feedback (textbox), marks (number).
+*/
+export function SubmissionForm() {}
 
-// export function ResourceForm() {}
+//////////////////////////////////////////////////////////////////////////////////////////
+/*
+  ResourceForm - for creating resources
+  fields: name (required, textbox), description (textbox), resourceURL (textbox), resourceFile (textbox), subjectType (required, selection[Course, Lab]), subject (required, selection[available course or lab]), uploadedBy (required, selection[available user]).
+*/
+export function ResourceForm() {}
--- src/Forms/InstituteForms.jsx
+++ src/Forms/InstituteForms.jsx
@@ -1,5 +1,17 @@
-// export function InstituteForm() {}
+/* InstituteForm - for creating institute
+  fields: name (required, textbox), instituteCode (required, textbox), contact (required, textbox), email (required, email), website (required, url), mobileNumber (required, number), address (required, textarea), addressCity (required, selection[based on state]), addressState (required, selection[based on country]), addressCountry (required, selection[only give India option]), addressPincode (required, number), establishedDate (required, date), accreditionDetails (textarea), instituteType (selection[central, state, private, autonomous, deemed-to-be]), universityAffiliation (textbox), principal, logo, missionStatement (textbox), visionStatement (textbox), campusArea (number), facilities (textarea)
+ */
 
-// export function DepartmentForm() {}
+export function InstituteForm() {}
 
-// export function GroupForm() {}
+/* DepartmentForm - for creating department
+  fields: name (required, textbox), departmentCode (required, textbox), departmentType (required, selection['Academic', 'Administrative', 'Student Service', 'Extracurricular and Co-curricular', 'Research and Development', 'Library and Learning Resources', 'Governance',]), headOfDepartment, institute.
+ */
+
+export function DepartmentForm() {}
+
+/* GroupForm - for creating group
+  fields: name (required, textbox), groupType (required, selection['Year', 'Semester', 'Division', 'Batch', 'Project', 'Tutorial', 'Elective', 'Mentor', 'Other',]), members (multiple selection [from avialable users]), groupIncharge, institute, department.
+ */
+
+export function GroupForm() {}
