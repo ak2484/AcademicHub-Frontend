802f852f230cd8e1fb90854396831a064a7a38e5
Rutvik Gondekar <gondekarrutvik@gmail.com>
2024-09-22 19:28:08 +0530

    change in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from './App.jsx'
-import Kamal from "./Kamal.jsx";
+import App from "./App.jsx";
+// import Kamal from "./Kamal.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
